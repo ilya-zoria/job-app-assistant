23ac0da7f691aee04137f04c15ed863cd9ef928c
Ilya <ilzorya@gmail.com>
2025-06-18 12:22:17 +0200

    style: simplify layout in Success component by removing Card wrapper and adding an image for visual enhancement

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,17 +1,15 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import { CheckCircle } from "lucide-react";
 import { Link } from "react-router-dom";
 
 export default function Success() {
   return (
-    <div className="bg-gradient-to-b from-muted-background to-background flex items-center justify-center p-4 w-full h-full">
-      <Card className="w-full max-w-md p-12 text-center bg-gradient-to-br from-primary/5 to-accent/5">
+    <div className="flex items-center justify-center p-4 w-full h-full">
+      <div className="w-full max-w-md p-12 text-center">
         <div className="flex flex-col items-center space-y-6">
-          <CheckCircle className="h-20 w-20 text-primary" />
-          <h1 className="text-4xl font-bold text-foreground">Welcome Aboard!</h1>
-          <p className="text-muted text-lg">
-            Thank you for joining our waitlist. We'll keep you updated on our progress and let you know as soon as we launch.
+          <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExd3l6eTV1YWV5a2U1cTZ0a3VraXY1amtza2g5Y29wNTBzbDRjMnVtcyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/hr4Ljjyj0L9RYlihLr/giphy.gif" alt="Success" className="w-60 h-60 object-contain mx-auto rounded-2xl" />
+          {/* <h1 className="text-5xl md:text-7xl italic leading-tight text-gray-900" style={{ fontFamily: 'Savate, sans-serif' }}>Welcome Aboard!</h1> */}
+          <p className="text-lg text-gray-500 mt-12 mb-8">Thank you for joining our waitlist. We'll keep you updated on our progress and let you know as soon as we launch.
           </p>
           <div className="pt-6">
             <Link to="/">
@@ -23,7 +21,7 @@ export default function Success() {
             </Link>
           </div>
         </div>
-      </Card>
+      </div>
     </div>
   );
 } 
